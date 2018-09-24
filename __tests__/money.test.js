7eae47dc3004a9b149d0faa12768769b181ce40a
Kenta Kase <kesin1202000@gmail.com>
2018-09-24 10:39:09 +0900

    Section 5

--- __tests__/money.test.js
+++ __tests__/money.test.js
@@ -1,4 +1,4 @@
-const { Dollar } = require('../lib/dollar')
+const { Dollar, Franc } = require('../lib/dollar')
 
 test('multiplication', () => {
   const five = new Dollar(5)
@@ -10,4 +10,11 @@ test('multiplication', () => {
 test('equality', () => {
   expect(new Dollar(5)).toEqual(new Dollar(5))
   expect(new Dollar(5)).not.toEqual(new Dollar(6))
+});
+
+test('franc multiplication', () => {
+  const five = new Franc(5)
+
+  expect(five.times(2)).toEqual(new Franc(10))
+  expect(five.times(3)).toEqual(new Franc(15))
 });
