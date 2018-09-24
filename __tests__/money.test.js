f0068fa54e3b0c05d98f6d4bfcae25cb50954cff
Kenta Kase <kesin1202000@gmail.com>
2018-09-24 10:34:11 +0900

    Section 3

--- __tests__/money.test.js
+++ __tests__/money.test.js
@@ -9,3 +9,8 @@ test('multiplication', () => {
   product = five.times(3)
   expect(product.amount).toBe(15)
 });
+
+test('equality', () => {
+  expect(new Dollar(5)).toEqual(new Dollar(5))
+  expect(new Dollar(5)).not.toEqual(new Dollar(6))
+});
